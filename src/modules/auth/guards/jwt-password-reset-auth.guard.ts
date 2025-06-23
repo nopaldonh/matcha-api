3a3b0ceb83562eec61d73813115adfcef2a7db48
nopaldonh <nopaldonh21@gmail.com>
2025-06-23 17:43:20 +0700

    feat: handle error jwt auth guard that the token is in request body

--- src/modules/auth/guards/jwt-password-reset-auth.guard.ts
+++ src/modules/auth/guards/jwt-password-reset-auth.guard.ts
@@ -1,7 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtPasswordResetAuthGuard extends AuthGuard(
-  'jwt-password-reset',
-) {}
+export class JwtPasswordResetAuthGuard extends AuthGuard('jwt-password-reset') {
+  handleRequest(err, user): any {
+    if (err || !user) {
+      throw (
+        err || new BadRequestException('Reset token is invalid or has expired')
+      );
+    }
+
+    return user;
+  }
+}
--- src/modules/auth/guards/jwt-verify-email-auth.guard.ts
+++ src/modules/auth/guards/jwt-verify-email-auth.guard.ts
@@ -1,5 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtVerifyEmailAuthGuard extends AuthGuard('jwt-verify-email') {}
+export class JwtVerifyEmailAuthGuard extends AuthGuard('jwt-verify-email') {
+  handleRequest(err, user): any {
+    if (err || !user) {
+      throw (
+        err ||
+        new BadRequestException('Verification token is invalid or has expired')
+      );
+    }
+
+    return user;
+  }
+}
