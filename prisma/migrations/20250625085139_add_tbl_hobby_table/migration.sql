9e7bef053deb635d536eac73f82a5967848598b7
nopaldonh <nopaldonh21@gmail.com>
2025-06-28 16:16:10 +0700

    fix: update prisma schema and migration to fix timestamp column in hobby model

--- prisma/migrations/20250625085139_add_tbl_hobby_table/migration.sql
+++ prisma/migrations/20250625085139_add_tbl_hobby_table/migration.sql
@@ -2,9 +2,9 @@
 CREATE TABLE `tbl_hobby` (
     `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(50) NOT NULL,
-    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updated_at` TIMESTAMP NOT NULL,
-    `deleted_at` TIMESTAMP NULL,
+    `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` TIMESTAMP(0) NOT NULL,
+    `deleted_at` TIMESTAMP(0) NULL,
 
     UNIQUE INDEX `tbl_hobby_name_key`(`name`),
     PRIMARY KEY (`id`)
